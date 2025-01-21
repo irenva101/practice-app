ace034af2fa173e5a0ea469b62dfc3a78dfc29d3
Irena Varga <irenva101@gmail.com>
2025-01-21 20:50:20 +0100

    upload radi

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -12,6 +12,35 @@ import { DragAndDropDirective } from '../directives/drag-and-drop.directive';
 })
 export class UploadComponent {
 
+  //data: BulkFileDataOut[] | undefined = [];
+  data: BulkFileDataOut[] = [
+    {
+        id: "1",
+        path: "/files/file1.txt",
+        dateTime: new Date('2025-01-21T10:00:00'),
+        status: FileStatus.Processing
+    },
+    {
+        id: "2",
+        path: "/files/file2.csv",
+        dateTime: new Date('2025-01-20T14:30:00'),
+        status: FileStatus.Processed
+    },
+    {
+        id: "3",
+        path: "/files/file3.pdf",
+        dateTime: new Date('2025-01-19T08:45:00'),
+        status: FileStatus.Failed
+    },
+    {
+        id: "4",
+        path: "/files/file4.jpg",
+        dateTime: new Date('2025-01-18T16:00:00'),
+        status: FileStatus.Processing
+    }
+];
+  isMoreIconClicked = false;
+
   onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
 
@@ -38,6 +67,7 @@ export class UploadComponent {
       //   });
     }
   }
+
   reloadTable(page?: number) {
     // this.client.queryFile(this.requestData).subscribe({
     //   next: (response) => {
@@ -50,6 +80,25 @@ export class UploadComponent {
     //   },
     // });
   }
+
+  moreIconClicked(){
+    this.isMoreIconClicked = !this.isMoreIconClicked;
+  }
+
+  downloadReport(idFile: string) {
+    //this.client.downloadImportReport(idFile, FileType.DeviceImport).subscribe({});
+  }
+
+  processFile(idFile: string) {
+    //this.client.processFile(idFile).subscribe({});
+  }
+  downloadFile(idFile: string) {
+    //this.client.downloadImportFile(idFile, FileType.DeviceImport).subscribe({});
+  }
+
+
+
+
 }
 
 
@@ -57,3 +106,39 @@ export interface FileParameter {
   data: any;
   fileName: string;
 }
+
+export class BulkFileDataOut implements IBulkFileDataOut {
+  id?: string;
+  path?: string | undefined;
+  dateTime?: Date | undefined;
+  status?: FileStatus;
+
+  constructor(data?: IBulkFileDataOut) {
+      if (data) {
+          for (var property in data) {
+              if (data.hasOwnProperty(property))
+                  (<any>this)[property] = (<any>data)[property];
+          }
+      }
+  }
+
+  
+
+  
+}
+
+export interface IBulkFileDataOut {
+  id?: string;
+  path?: string | undefined;
+  dateTime?: Date | undefined;
+  status?: FileStatus;
+}
+
+export enum FileStatus {
+  NotProcessed = "NotProcessed",
+  Processing = "Processing",
+  Processed = "Processed",
+  Failed = "Failed",
+}
+
+
